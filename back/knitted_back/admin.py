b144d9cd80729dff7e24732cb8a810465d7a0ce2
daskalidan <idandaskal@gmail.com>
2022-07-24 13:47:30 +0300

    super user, product test

--- back/knitted_back/admin.py
+++ back/knitted_back/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Category, Product
+
+# from library_back.models import Book
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+
+# superuser username:theboss,email:the@boss.com,password:abc123
--- back/knitted_back/urls.py
+++ back/knitted_back/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('home', views.home),
     path('latest-products/', views.LatestProductsList.as_view())
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
