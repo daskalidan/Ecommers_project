b72cf22a08d80f16154da4edab98fe49a59cc4b2
daskalidan <idandaskal@gmail.com>
2022-07-24 13:22:04 +0300

    product, category models. latest products view.

--- back/knitted_back/urls.py
+++ back/knitted_back/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('home', views.home),
+    path('latest-products/', views.LatestProductsList.as_view())
 ]
--- back/knitted_back/views.py
+++ back/knitted_back/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
 def home(request):
     pass
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
